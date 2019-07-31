f637ea1ca58819eaa9a0eb3ca110e79469a9ac33
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-07-31 22:14:10 +0300

    Change task js and css, remove old depend

--- gulpfile.js
+++ gulpfile.js
@@ -1,100 +1,103 @@
-const gulp = require('gulp'),
-	rigger = require('gulp-rigger'),
-	prefixer = require('gulp-autoprefixer'),
-	del = require('del'),
-	sass = require('gulp-sass'),
-	concat = require('gulp-concat'),
-	minify = require('gulp-minify'),
-	browserSync = require('browser-sync'),
-	rollup = require('gulp-better-rollup'),
-	sourcemaps = require('gulp-sourcemaps'),
-	plumber = require('gulp-plumber'),
-	reload = browserSync.reload,
-	svgSprite = require('gulp-svg-sprite'),
-	svgmin = require('gulp-svgmin'),
-	cheerio = require('gulp-cheerio'),
-	replace = require('gulp-replace');
+const gulp = require('gulp');
+const prefixer = require('gulp-autoprefixer');
+const del = require('del');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const clearSCC = require('gulp-clean-css');
+const browserSync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const svgSprite = require('gulp-svg-sprite');
+const svgmin = require('gulp-svgmin');
+const cheerio = require('gulp-cheerio');
+const replace = require('gulp-replace');
+const resolve = require('rollup-plugin-node-resolve');
+const posthtml = require('gulp-posthtml');
+const include = require('posthtml-include');
+const rollup = require('rollup');
+const postcss = require('gulp-postcss');
+const cssmqpacker = require('css-mqpacker');
 
-//Таск browser-sync
-gulp.task('browser-sync', function() {
+const { reload } = browserSync;
+
+gulp.task('browser-sync', () => {
 	browserSync.init({
+		ghostMode: {
+			clicks: true,
+			forms: true,
+			scroll: false
+		},
 		server: {
 			baseDir: './build'
 		}
 	});
 });
 
-//Перенос Html в build
 gulp.task('html:build', function() {
 	return gulp
 		.src('./src/*.html')
-		.pipe(rigger())
+		.pipe(posthtml([include({ encoding: 'utf8' })]))
 		.pipe(gulp.dest('./build'))
 		.pipe(reload({ stream: true }));
 });
 
-//Очистка build
 gulp.task('clean', function() {
 	return del(['build']);
 });
-//Js rollup
-gulp.task('js:rollup', function() {
-	return gulp
-		.src('src/js/main.js')
-		.pipe(plumber())
-		.pipe(sourcemaps.init())
-		.pipe(rollup({}, 'iife'))
-		.pipe(sourcemaps.write(''))
-		.pipe(gulp.dest('build/js'));
+
+gulp.task('js:rollup', async function() {
+	const bundle = await rollup.rollup({
+		input: 'src/js/main.js',
+		plugins: [resolve()]
+	});
+	await bundle.write({
+		file: 'build/js/main.js',
+		format: 'iife',
+		name: 'library',
+		sourcemap: true
+	});
 });
-//Перенос библиотек JS
+
 gulp.task('js-common:build', function() {
 	return gulp.src('./src/js-common/**/*.js').pipe(gulp.dest('./build/js/'));
 });
 
-//Таск по стилям
+gulp.task('favicon', function() {
+	return gulp.src('./src/*.ico').pipe(gulp.dest('./build/'));
+});
+
 gulp.task('sass', function() {
+	const plugins = [cssmqpacker()];
+
 	return gulp
-		.src('./src/scss/**/*.scss')
+		.src('./src/scss/main.scss')
 		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(replace(/^[ \t]*\@charset[ \t]+\"UTF\-8\"[ \t]*;/gim, ''))
+		.pipe(concat('style.css'))
 		.pipe(
 			prefixer({
-				browsers: [
+				overrideBrowserslist: [
 					'last 3 version',
 					'> 1%',
-					'ie 8',
-					'ie 9',
-					'Opera 12.1',
-					'Firefox >= 2'
+					'ie 10',
+					'maintained node versions'
 				],
 				cascade: false
 			})
 		)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(concat('style.css'))
-		.pipe(
-			minify({
-				ext: {
-					src: '',
-					min: '.min.css'
-				},
-				noSource: false
-			})
-		)
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('./build/css'));
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest('./build/css'))
+		.pipe(reload({ stream: true }));
 });
 
-//Шрифты
 gulp.task('fonts:build', function() {
 	return gulp.src('./src/fonts/**/*.*').pipe(gulp.dest('./build/fonts/'));
 });
 
-//Изображения растр
 gulp.task('img:build', function() {
 	return gulp.src('./src/img/other/*.*').pipe(gulp.dest('./build/images/'));
 });
-//Изображения SVG
+
 gulp.task('sprite:svg', function() {
 	return gulp
 		.src('./src/img/svg/*.svg')
@@ -128,17 +131,15 @@ gulp.task('sprite:svg', function() {
 		.pipe(gulp.dest('./build/images/'));
 });
 
-//Отслеживание изменений
-gulp.task('watch', function() {
+gulp.task('watch', () => {
 	gulp.watch('./src/**/*.html', gulp.series('html:build'));
 	gulp.watch('./src/js/**/*.js', gulp.series('js:rollup'));
 	gulp.watch('./src/js-common/**/*.js', gulp.series('js-common:build'));
-	gulp.watch('./src/scss/**/*.scss', gulp.series('sass'));
+	gulp.watch('./src/**/*.scss', gulp.series('sass'));
 	gulp.watch('./src/img/other/*.*', gulp.series('img:build'));
 	gulp.watch('./src/img/svg/*.*', gulp.series('img:build'));
 });
 
-//Дефолтный таск
 gulp.task(
 	'default',
 	gulp.series(
@@ -150,7 +151,8 @@ gulp.task(
 			'sass',
 			'fonts:build',
 			'img:build',
-			'sprite:svg'
+			'sprite:svg',
+			'favicon'
 		),
 		gulp.parallel('watch', 'browser-sync')
 	)
