0f04416942ad543b3ea1c9fb3202788cbc035792
Sergey Razdolyansy <grafcorp01@gmail.com>
2019-10-23 21:40:46 +0300

    Add sass task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const bSync = require('browser-sync');
 const del = require('del');
-
+const sourcemaps = require('gulp-sourcemaps');
+// Graphic
+const rename = require('gulp-rename');
 // Html
 const posthtml = require('gulp-posthtml');
 const include = require('posthtml-include');
@@ -11,16 +13,20 @@ const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
 const { uglify } = require('rollup-plugin-uglify');
+// CSS
+const cssmqpacker = require('css-mqpacker');
+const postcss = require('gulp-postcss');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const csso = require('gulp-csso');
+const prefixer = require('gulp-autoprefixer');
 
 // Entrypoint
 const configPath = require('./config.entrypoint');
 
 // const prefixer = require('gulp-autoprefixer');
 //
-// const sass = require('gulp-sass');
 // const concat = require('gulp-concat');
-// const browserSync = require('browser-sync');
-// const sourcemaps = require('gulp-sourcemaps');
 // const svgSprite = require('gulp-svg-sprite');
 // const svgmin = require('gulp-svgmin');
 // const cheerio = require('gulp-cheerio');
@@ -85,89 +91,44 @@ gulp.task('favicon', () =>
 	gulp.src('./src/*.ico').pipe(gulp.dest(configPath.dir))
 );
 
-// gulp.task('sass', function() {
-// 	const plugins = [cssmqpacker()];
-
-// 	return gulp
-// 		.src('./src/scss/main.scss')
-// 		.pipe(sourcemaps.init())
-// 		.pipe(sass().on('error', sass.logError))
-// 		.pipe(replace(/^[ \t]*\@charset[ \t]+\"UTF\-8\"[ \t]*;/gim, ''))
-// 		.pipe(concat('style.css'))
-// 		.pipe(
-// 			prefixer({
-// 				overrideBrowserslist: [
-// 					'last 3 version',
-// 					'> 1%',
-// 					'ie 10',
-// 					'maintained node versions'
-// 				],
-// 				cascade: false
-// 			})
-// 		)
-// 		.pipe(postcss(plugins))
-// 		.pipe(gulp.dest('./build/css'))
-// 		.pipe(reload({ stream: true }));
-// });
-
 gulp.task('font', () => {
 	return gulp
 		.src(configPath.font.entry)
 		.pipe(gulp.dest(configPath.font.output));
 });
 
-// gulp.task('img:build', function() {
-// 	return gulp.src('./src/img/other/*.*').pipe(gulp.dest('./build/images/'));
-// });
+gulp.task('sass', () => {
+	const plugins = [cssmqpacker()];
+	return gulp
+		.src(configPath.css.entry)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(concat(configPath.css.nameFile))
+		.pipe(postcss(plugins))
+		.pipe(gulp.dest(configPath.css.output))
+		.pipe(reload({ stream: true }));
+});
 
-// gulp.task('sprite:svg', function() {
-// 	return gulp
-// 		.src('./src/img/svg/*.svg')
-// 		.pipe(
-// 			svgmin({
-// 				js2svg: {
-// 					pretty: true
-// 				}
-// 			})
-// 		)
-// 		.pipe(
-// 			cheerio({
-// 				run: function($) {
-// 					$('[fill]').removeAttr('fill');
-// 					$('[stroke]').removeAttr('stroke');
-// 					$('[style]').removeAttr('style');
-// 				},
-// 				parserOptions: { xmlMode: true }
-// 			})
-// 		)
-// 		.pipe(replace('&gt;', '>'))
-// 		.pipe(
-// 			svgSprite({
-// 				mode: {
-// 					symbol: {
-// 						sprite: '../sprite-vector.svg'
-// 					}
-// 				}
-// 			})
-// 		)
-// 		.pipe(gulp.dest('./build/images/'));
-// });
+gulp.task('img', () => {
+	return gulp
+		.src(configPath.img.entry)
+		.pipe(rename({ dirname: '' }))
+		.pipe(gulp.dest(configPath.img.output));
+});
 
 gulp.task('watch', () => {
 	gulp.watch(configPath.html.watch, gulp.series('html'));
 	gulp.watch(configPath.js.watch, gulp.series('js'));
 	gulp.watch(configPath.font.watch, gulp.series('font'));
-	// gulp.watch('./src/js-common/**/*.js', gulp.series('js-common:build'));
-	// gulp.watch('./src/**/*.scss', gulp.series('sass'));
-	// gulp.watch('./src/img/other/*.*', gulp.series('img:build'));
-	// gulp.watch('./src/img/svg/*.*', gulp.series('img:build'));
+	gulp.watch(configPath.img.watch, gulp.series('img'));
+	gulp.watch(configPath.css.watch, gulp.series('sass'));
 });
 
 gulp.task(
 	'default',
 	gulp.series(
 		'clean',
-		gulp.parallel('html', 'js', 'favicon', 'font'),
+		gulp.parallel('html', 'js', 'favicon', 'font', 'img', 'sass'),
 		gulp.parallel('watch', 'bSync')
 	)
 );
